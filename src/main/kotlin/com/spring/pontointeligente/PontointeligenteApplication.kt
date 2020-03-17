dd144523607d5563cd9747a6a05fbca7f2f8ffce
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-03-16 23:25:17 -0300

    chore:start project

---
+++ src/main/kotlin/com/spring/pontointeligente/PontointeligenteApplication.kt
@@ -0,0 +1,11 @@
+package com.spring.pontointeligente
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PontointeligenteApplication
+
+fun main(args: Array<String>) {
+	runApplication<PontointeligenteApplication>(*args)
+}
