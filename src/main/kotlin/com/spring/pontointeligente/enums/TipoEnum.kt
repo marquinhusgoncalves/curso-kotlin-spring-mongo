4c75ce710f56bae1231f6f7206ec9b0a6f154dcf
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-03-17 00:52:02 -0300

    chore:create data class of the type documents

---
+++ src/main/kotlin/com/spring/pontointeligente/enums/TipoEnum.kt
@@ -0,0 +1,10 @@
+package com.spring.pontointeligente.enums
+
+enum class TipoEnum {
+    INICIO_TRABALHO,
+    TERMINO_TRABALHO,
+    INICIO_ALMOCCO,
+    TERMINO_ALMOCO,
+    INICIO_PAUSA,
+    TERMINO_PAUSA
+}
